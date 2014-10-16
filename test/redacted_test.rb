b94d10a13ec49c4525bd11f0cde59ca018a4d24a
Kurt Funai <jesuiskurt@gmail.com>
2014-10-15 23:19:49 -0400

    Added some tests to ensure that the Rails Engine and Railties are correctly being loaded. Closes #1

--- test/redacted_test.rb
+++ test/redacted_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
-class RedactedTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Redacted
+class RedactedTest < ActionDispatch::IntegrationTest
+  test "redacted stylesheet is available" do
+    get "/assets/redacted.css"
+    assert_response(:success)
+    assert_match ".redacted-text", @response.body
+  end
+
+  test "engine is loaded" do
+    assert_equal ::Rails::Engine, Redacted::Engine.superclass
+  end
+
+  test "Redacted::ActionView::Helpers are loaded" do
+    assert(Redacted::ActionView::Helpers.present?, 'Redacted::ActionView::Helpers are not defined')
+  end
+
+  test "method redact_html is defined" do
+    assert(Redacted::ActionView::Helpers.method_defined?(:redact_html), '#redact_html is not defined')
   end
 end
