f633465f29c475760de7d09a82fafc4c71770c04
Kurt Funai <jesuiskurt@gmail.com>
2014-10-16 00:31:24 -0400

    Created tests for the #redact_html helper method

--- lib/redacted/action_view/helpers.rb
+++ lib/redacted/action_view/helpers.rb
@@ -8,9 +8,14 @@ module Redacted
 
       def redact_html str
         return '' unless str
+        "<span class=\"redacted-text\">#{generate_fake_text(str)}</span>".html_safe
+      end
+
+      def generate_fake_text(str)
         word_count = str.split.size
         fake_text = Faker::Lorem.words(word_count).join(' ')
-        "<span class=\"redacted-text\">#{fake_text.slice(0, str.length)}</span>".html_safe
+        fake_text << Faker::Lorem.characters(str.length - fake_text.length) if str.length > fake_text.length
+        fake_text.slice(0, str.length)
       end
     end
   end
--- test/redacted_test.rb
+++ test/redacted_test.rb
@@ -1,21 +1,49 @@
 require 'test_helper'
 
 class RedactedTest < ActionDispatch::IntegrationTest
-  test "redacted stylesheet is available" do
+  test 'redacted stylesheet is available' do
     get "/assets/redacted.css"
     assert_response(:success)
     assert_match ".redacted-text", @response.body
   end
 
-  test "engine is loaded" do
+  test 'engine is loaded' do
     assert_equal ::Rails::Engine, Redacted::Engine.superclass
   end
 
-  test "Redacted::ActionView::Helpers are loaded" do
-    assert(Redacted::ActionView::Helpers.present?, 'Redacted::ActionView::Helpers are not defined')
+  test 'Redacted::ActionView::Helpers is loaded' do
+    assert(Redacted::ActionView::Helpers.present?, 'Redacted::ActionView::Helpers is not defined')
   end
 
-  test "method redact_html is defined" do
+  test 'helper method redact_html is defined' do
     assert(Redacted::ActionView::Helpers.method_defined?(:redact_html), '#redact_html is not defined')
+  end
+end
+
+class RedactedHelpersTest < ActionView::TestCase
+  include Redacted::ActionView::Helpers
+
+  test '#redact_html returns string of random text' do
+    phrase = "Canada's maple syrup was stolen by grizzly bears"
+    html = redact_html(phrase)
+    refute_equal redacted_text(html), phrase
+    refute redacted_text(html).include?(phrase)
+  end
+
+  test '#redact_html returns string of same length' do
+    phrase = "Canada's maple syrup was stolen by grizzly bears"
+    html = redact_html(phrase)
+    assert_equal phrase.length, redacted_text(html).length
+  end
+
+  test '#redact_html creates span tags with class .redacted-text' do
+    html = redact_html("Shakedown 1979")
+    assert html.include?('<span class="redacted-text">')
+    assert html.include?('</span>')
+  end
+
+  private
+  def redacted_text(redacted_html)
+    strip_tags(redacted_html)
   end
 end
