8f4d63d273a5e2c6cbc7efba8ae0afa1a83ae29f
Kurt Funai <jesuiskurt@gmail.com>
2014-10-26 14:14:29 -0400

    Cleaning up tests.

--- test/redacted_helper_test.rb
+++ test/redacted_helper_test.rb
@@ -6,7 +6,8 @@ class RedactedHelpersTest < ActionView::TestCase
   def setup
     @phrase = "Canada's maple syrup was stolen by grizzly bears"
     @paragraphs = "This one paragraph.\nThis is two.\n\nThis is three!\r\nThis is four?"
-    @redact_text = redact_text(@phrase)
+    @redacted_text = redact_text(@phrase)
+    @redacted_paragraphs = redact_paragraphs(@paragraphs)
   end
 
   test '#redact returns single line when passed string without characters' do
@@ -14,31 +15,32 @@ class RedactedHelpersTest < ActionView::TestCase
   end
 
   test '#redact splits into paragraphs when passed paragraphs' do
-    assert redact(@paragraphs).include?('<p>'), "redact paragraphs should have p tags"
+    assert redact(@paragraphs).include?('<p>'), "redact_paragraphs should have p tags"
     assert redact(@paragraphs).include?('</p>')
   end
 
-  test '#redact_paragraphs returns p tags around content' do
-    assert redact_paragraphs(@paragraphs).first.include?('<p>')
-    assert redact_paragraphs(@paragraphs).first.include?('</p>')
+  test '#redact_paragraphs returns array of p tags around content' do
+    assert @redacted_paragraphs.first.include?('<p>')
+    assert @redacted_paragraphs.first.include?('</p>')
   end
 
-  test '#redact_paragraphs splits into correct number of paragraphs returned' do
-    assert_equal 4, redact_paragraphs(@paragraphs).size
+  test '#redact_paragraphs returns array with correct number of paragraphs returned' do
+    assert @redacted_paragraphs.class.name == "Array", "redact_paragraphs should be an array"
+    assert_equal 4, @redacted_paragraphs.size, "redacted_paragraphs should return proper number of paragraphs"
   end
 
   test '#redact_text returns string of random text' do
-    refute_equal html_text(@redact_text), @phrase
-    refute html_text(@redact_text).include?(@phrase)
+    refute_equal html_text(@redacted_text), @phrase
+    refute html_text(@redacted_text).include?(@phrase)
   end
 
   test '#redact_text returns string of same length' do
-    assert_equal @phrase.length, html_text(@redact_text).length
+    assert_equal @phrase.length, html_text(@redacted_text).length
   end
 
   test '#redact_text creates span tags with class .redacted-text' do
-    assert @redact_text.include?('<span class="redacted-text">')
-    assert @redact_text.include?('</span>')
+    assert @redacted_text.include?('<span class="redacted-text">')
+    assert @redacted_text.include?('</span>')
   end
 
   test '#ascii_redact returns ascii blocks of same length' do
